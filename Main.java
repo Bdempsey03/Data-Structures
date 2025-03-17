3f7a9ddd8aa8c4d2d9aff3dbb53d8283122afcfa
Bdempsey03 <bdempsey@unbc.ca>
2025-03-17 16:45:36 -0700

    Added to main

--- Main.java
+++ Main.java
@@ -1,11 +1,29 @@
+import java.io.File;
+
 public class Main{
+
+
+
+    private static SparseMatrix input; //has getRow, getVal, getCol
+    private static int[][] tarjanOutput; //output of Tarjan algorithm
+
+
+    private void permuteMatrix(String file){
+
+        //permute the adjacency matrix while maintaining the order of the strongly connected components
+        input = new SparseMatrix(new File(file));
+        
+    }
+
 
     public static void main(String[] args) {
         
-
+        
+        
         //Take the output of the Tarjan algorithm and permute the adjacency matrix while maintaining the order of the strongly connected components
         
         
+
     
     }
 }
--- SparseMatrix.java
+++ SparseMatrix.java
@@ -1,3 +1,9 @@
+import java.io.File;
+
 public class SparseMatrix{
     //store in compressed sparse column format
+
+    public SparseMatrix(File file){
+        //read the file and store the matrix in compressed sparse column format
+    }
 }
