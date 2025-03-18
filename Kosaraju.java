636c64eaba4ad3f7a66178d5f7c3700d19825c05
GoldenButterMan <128435596+GoldenButterMan@users.noreply.github.com>
2025-03-18 12:16:27 -0700

    Got code working. added main method to test out code

--- Kosaraju.java
+++ Kosaraju.java
@@ -9,7 +9,7 @@ public class Kosaraju {
     //Depth First search
     private void depthFirst(int value, boolean[] visited, Stack<Integer> stack){
         visited[value] = true;
-        for(int i = 0; i < value; i++){
+        for(int i = 0; i < adjacency.length; i++){
             if(adjacency[value][i] == 1 && !visited[i]){
                 depthFirst(i, visited, stack);
             }
@@ -20,25 +20,69 @@ public class Kosaraju {
     }
 
     //Transpose Matrix
-    private int[][] transposeMatrix(int[][] transpose){
-        for(int i = 0; i < transpose.length; i++){
-            for(int j = i+1; j < transpose.length; j++){
-                int temp = transpose[i][j];
-                transpose[i][j] = transpose[j][i];
-                transpose[j][i] = temp;
+    private Kosaraju transposeMatrix(int[][] base){
+        int[][] transpose = new int[base.length][base.length];
+        for(int i = 0; i < adjacency.length; i++){
+            for(int j = i+1; j < adjacency.length; j++){
+                transpose[i][j] = base[j][i];
             }
         }
-        return transpose;
+
+        Kosaraju transposedVer = new Kosaraju(transpose);
+        return transposedVer;
     }
     
     //Get Strongly connected components
     private void getSCC(int value, boolean[] visited, List<Integer> SCC){
         visited[value] = true;
         SCC.add(value);
-        for(int i = 0; i < value; i++){
-            if(!visited[i]){
+        for(int i = 0; i < adjacency.length; i++){
+            if(adjacency[value][i] == 1 && !visited[i]){
                 getSCC(i, visited, SCC);
             }
+        }
+    }
+
+    public List<List<Integer>> runAlgorithm(){
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[adjacency.length];
+
+        for(int i = 0; i < adjacency.length; i++){
+            if(!visited[i]){
+                depthFirst(i, visited, stack);
+            }
+        }
+
+        Kosaraju transpose = transposeMatrix(adjacency);
+
+        Arrays.fill(visited, false);
+
+        List<List<Integer>> stronglyConnectedComponents = new ArrayList<>();
+
+        while(!stack.isEmpty()){
+            int value = stack.pop();
+            if(!visited[value]){
+                List<Integer> scc = new ArrayList<>();
+                transpose.getSCC(value, visited, scc);
+                stronglyConnectedComponents.add(scc);
+            }
+        }
+        return stronglyConnectedComponents;
+    }
+
+    public static void main(String[] args){
+        int[][] test = {
+            {0, 1, 0, 0, 0}, 
+            {0, 0, 1, 0, 0}, 
+            {0, 0, 0, 1, 0}, 
+            {1, 0, 0, 0, 0}, 
+            {0, 0, 0, 1, 0}
+        };
+        Kosaraju testImplement = new Kosaraju(test);
+        List<List<Integer>> stronglyConnectedComponents = testImplement.runAlgorithm();
+        System.out.println("SCCs: ");
+        for(List<Integer>scc : stronglyConnectedComponents){
+            System.out.println(scc);
         }
     }
 }
