6d7d832122bfa30299cf5aa3a5fd0464da4ec995
GoldenButterMan <spencer-peterson@live.com>
2025-03-22 17:05:00 -0700

    Modified Kosaraju to handle CSC Adjacency Matrices

--- Kosaraju.java
+++ Kosaraju.java
@@ -1,17 +1,20 @@
 import java.util.*;
 public class Kosaraju {
-    private int[][] adjacency;
+    private int[] rowIndices;
+    private int[] colPtrs;
 
-    public Kosaraju(int[][] adjacency){
-        this.adjacency = adjacency;
+    public Kosaraju(int[] rowIndices, int[] colPtrs){
+        this.rowIndices = rowIndices;
+        this.colPtrs = colPtrs;
     }
 
     //Depth First search
     private void depthFirst(int value, boolean[] visited, Stack<Integer> stack){
         visited[value] = true;
-        for(int i = 0; i < adjacency.length; i++){
-            if(adjacency[value][i] == 1 && !visited[i]){
-                depthFirst(i, visited, stack);
+        for(int i = colPtrs[value]; i < colPtrs[value + 1]; i++){
+            int neighbor = rowIndices[i];
+            if(!visited[neighbor]){
+                depthFirst(neighbor, visited, stack);
             }
         }
 
@@ -20,15 +23,27 @@ public class Kosaraju {
     }
 
     //Transpose Matrix
-    private Kosaraju transposeMatrix(int[][] base){
-        int[][] transpose = new int[base.length][base.length];
-        for(int i = 0; i < adjacency.length; i++){
-            for(int j = i+1; j < adjacency.length; j++){
-                transpose[i][j] = base[j][i];
+    private Kosaraju transposeMatrix(){
+        int[] transposedRow = new int[rowIndices.length];
+        int[] transposedColPtrs = new int[(colPtrs.length - 1) + 1];
+        //Count occurneces of row indices
+        for(int i : rowIndices){
+            transposedColPtrs[i + 1]++;
+        }
+        //Compute colPtr offsets
+        for(int i = 1; i <= (colPtrs.length - 1); i++){
+            transposedColPtrs[i] += transposedColPtrs[i - 1];
+        }
+        //Fill row indices for tranposed matrix
+        int[] position = Arrays.copyOf(transposedColPtrs, transposedColPtrs.length);
+        for(int column = 0; column < (colPtrs.length - 1); column++){
+            for(int j = colPtrs[column]; j < colPtrs[column + 1]; j++){
+                int row = rowIndices[j];
+                transposedRow[position[row]++] = column;
             }
         }
-
-        Kosaraju transposedVer = new Kosaraju(transpose);
+        //return transposed version of CSC matrix
+        Kosaraju transposedVer = new Kosaraju(transposedRow, transposedColPtrs);
         return transposedVer;
     }
     
@@ -36,24 +51,25 @@ public class Kosaraju {
     private void getSCC(int value, boolean[] visited, List<Integer> SCC){
         visited[value] = true;
         SCC.add(value);
-        for(int i = 0; i < adjacency.length; i++){
-            if(adjacency[value][i] == 1 && !visited[i]){
-                getSCC(i, visited, SCC);
+        for(int i = colPtrs[value]; i < colPtrs[value + 1]; i++){
+            int neighbor = rowIndices[i];
+            if(!visited[neighbor]){
+                getSCC(neighbor, visited, SCC);
             }
         }
     }
 
     public List<List<Integer>> runAlgorithm(){
         Stack<Integer> stack = new Stack<>();
-        boolean[] visited = new boolean[adjacency.length];
+        boolean[] visited = new boolean[colPtrs.length - 1];
 
-        for(int i = 0; i < adjacency.length; i++){
+        for(int i = 0; i < (colPtrs.length - 1); i++){
             if(!visited[i]){
                 depthFirst(i, visited, stack);
             }
         }
 
-        Kosaraju transpose = transposeMatrix(adjacency);
+        Kosaraju transpose = transposeMatrix();
 
         Arrays.fill(visited, false);
 
@@ -71,14 +87,9 @@ public class Kosaraju {
     }
 
     public static void main(String[] args){
-        int[][] test = {
-            {0, 1, 0, 0, 0}, 
-            {0, 0, 1, 0, 0}, 
-            {0, 0, 0, 1, 0}, 
-            {1, 0, 0, 0, 0}, 
-            {0, 0, 0, 1, 0}
-        };
-        Kosaraju testImplement = new Kosaraju(test);
+        int[] testRowIndices = {1, 2, 0, 3, 4, 3};  // Outgoing edges (row indices)
+        int[] testColPointers = {0, 1, 2, 4, 5, 6}; // Column start positions
+        Kosaraju testImplement = new Kosaraju(testRowIndices, testColPointers);
         List<List<Integer>> stronglyConnectedComponents = testImplement.runAlgorithm();
         System.out.println("SCCs: ");
         for(List<Integer>scc : stronglyConnectedComponents){
